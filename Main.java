070d281d6f1bcae17ceeaa54060c90adc1a8a5b4
PRECIOUSAJILOR2 <86179834+precious010@users.noreply.github.com>
2021-11-19 15:24:27 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,21 @@
+import java.io.IOException;
+import java.io.File;
+import java.io.FileWriter;
+import java.util.Scanner;
+import java.io.*;
+
+
+
+
+class Main {
+  public static void main(String[] args) {
+    
+    Scanner input = new Scanner(System.in);
+    System.out.println("Please enter a valid integer");
+
+
+
+
+    System.out.println("Hello world!");
+  }
+}
